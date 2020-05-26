ff16fc7c8e6cbff8fc75e2e8d14fc1c47b1aa76b
Chen, Winston <Winston.Chen@activenetwork.com>
2020-05-26 18:01:21 +0800

    add jdk practice

---
+++ jdk-practice/src/main/java/com/winston/practice/jdk/socket/ServerSocketTest.java
@@ -0,0 +1,40 @@
+package com.winston.practice.jdk.socket;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ServerSocketTest {
+
+    public static void main(String[] args) throws IOException {
+
+        ExecutorService s = Executors.newCachedThreadPool();
+
+        try (ServerSocket socket = new ServerSocket(9999, 1)) {
+            System.out.println("启动成功了，等待连接");
+            while (true) {
+                //accept 从队列里面获取一个请求  建立一个对应的socket
+                Socket accept = socket.accept();
+                System.out.println(Thread.currentThread().getName() + "取得连接");
+                //启动一个线程  来处理当前请求的socket
+                s.submit(() -> {
+                    try (InputStream inputStream = accept.getInputStream();) {
+                        System.out.println(Thread.currentThread().getName() + "获取了输入流");
+                        byte[] buffer = new byte[1024];
+                        int len;
+                        while ((len = inputStream.read(buffer)) != -1) {
+                            String content = new String(buffer, 0, len);
+                            System.out.println(Thread.currentThread().getName() + "读取到数据：" + content);
+                        }
+                        System.out.println(Thread.currentThread().getName() + "结束了");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+            }
+        }
+    }
+}
