7b00844ed10854e898dde4556b6f144df94b3a71
Chen, Winston <Winston.Chen@activenetwork.com>
2020-07-30 15:09:48 +0800

    integer

--- jdk-practice/src/main/java/com/winston/practice/jdk/number/NUmberTest2.java
+++ jdk-practice/src/main/java/com/winston/practice/jdk/number/NUmberTest2.java
@@ -6,19 +6,25 @@ public class NUmberTest2 {
 
 
         //A
-        int i1 = 127;
-        int i2 = 127;
-        System.out.printf("i1~%d\ti2~%d\n", System.identityHashCode(i1), System.identityHashCode(i2));
-
+        Integer i1 = 127;
+        Integer i2 = 127;
+        //定义的 i1  和 i2  通过基础类型赋值 会自动进行装箱操作
+        //使用javap -c NUmberTest2
+        //通过反编译结果  可以看到 Integer 自动装箱是通过调用 Integer.valueOf() 来完成的
+        // （我不知道为啥子 Idea的反编译 不限显示Integer.valueOf()  这个装箱操作 猜测是idea反编译 会优化显示结果）
+        //通过源码  可以看到 Integer.valueOf 里面 有IntegerCache 对象 该对象 缓存了 -128 到 127 的实例，超过范围的会通过构造方法新创建
+        //所以此处 结果 为 true
+        System.out.println(i1 == i2);
         //B
-        int i3 = 128;
-        int i4 = 128;
-        System.out.printf("i3~%d\ti4~%d\n", System.identityHashCode(i3), System.identityHashCode(i4));
+        Integer i3 = 128;
+        Integer i4 = 128;
+        //所以此处 为 false
+        System.out.println(i3 == i4);
 
         //C
-        int i5 = 235;
-        int i6 = i5;
-        System.out.printf("i5~%d\ti6~%d\n", System.identityHashCode(i5), System.identityHashCode(i6));
+        Integer i5 = 235;
+        Integer i6 = i5;
+        System.out.println(i5 == i6);
 
 
     }
--- jdk-practice/src/main/java/com/winston/practice/jdk/number/NUmberTest3.java
+++ jdk-practice/src/main/java/com/winston/practice/jdk/number/NUmberTest3.java
@@ -3,23 +3,39 @@ package com.winston.practice.jdk.number;
 public class NUmberTest3 {
 
     public static void main(String[] args) {
+        Integer i1 = 40;
+        Integer i2 = 40;
+        Integer i3 = 0;
+        Integer i4 = new Integer(40);
+        Integer i5 = new Integer(40);
+        Integer i6 = new Integer(0);
+
+        System.out.println("i1=i2   " + (i1 == i2));
+        System.out.println("i1=i2+i3   " + (i1 == i2 + i3));
+        System.out.println("i1=i4   " + (i1 == i4));
+        System.out.println("i4=i5   " + (i4 == i5));
+        // i5+i6 会将 i5 和 i6 继续拆箱 通过 int类型继续 + 运算 得到结果 为 int 类型
+        // 然后 i4 是Integer 类型 不能和 int 直接进行 == 运算 ，jvm 会将 i4 也进行 拆箱 再进行运算 ，所以下面结果为 true
+        //具体可以参看 下面反编译的结果
+        System.out.println("i4=i5+i6   " + (i4 == i5 + i6));
+        System.out.println("40=i5+i6   " + (40 == i5 + i6));
 
 
-        //A
-        int i1 = 127;
-        int i2 = 127;
-        System.out.println(i1 == i2);
-
-        //B
-        int i3 = 128;
-        int i4 = 128;
-        System.out.println(i3 == i4);
-
-        //C
-        int i5 = 235;
-        int i6 = i5;
-        System.out.printf("i5~%d\ti6~%d\n", System.identityHashCode(i5), System.identityHashCode(i6));
-
+        /**
+         *      反编译结果：
+         *      Integer i1 = Integer.valueOf(40);
+         *       Integer i2 = Integer.valueOf(40);
+         *       Integer i3 = Integer.valueOf(0);
+         *       Integer i4 = new Integer(40);
+         *       Integer i5 = new Integer(40);
+         *       Integer i6 = new Integer(0);
+         *       System.out.println("i1=i2   " + (i1 == i2));
+         *       System.out.println("i1=i2+i3   " + (i1.intValue() == i2.intValue() + i3.intValue()));
+         *       System.out.println("i1=i4   " + (i1 == i4));
+         *       System.out.println("i4=i5   " + (i4 == i5));
+         *       System.out.println("i4=i5+i6   " + (i4.intValue() == i5.intValue() + i6.intValue()));
+         *       System.out.println("40=i5+i6   " + (40 == i5.intValue() + i6.intValue()));
+         */
 
     }
 }
