f941022781895547fb4588d654bf83fa2c1a2203
wchen <winston.chen@activenetwork.com>
2021-06-11 10:14:36 +0800

    map

--- jdk-practice/src/main/java/com/winston/practice/jvm/classload/TestArrayMemoryAllocation.java
+++ jdk-practice/src/main/java/com/winston/practice/jvm/classload/TestArrayMemoryAllocation.java
@@ -13,8 +13,8 @@ public class TestArrayMemoryAllocation {
     }
     static void testBoolean(){
         //4956-3932=1024k  此处并不代表boolean占用一个字节，
-        //jvm并未定义 boolean 这种类型 所以 boolean平时会被转为int类型 所以占用4个字节，此处boolean的数组会被转为byte数组，所以占用一个字节
-
+        //在Java虚拟机中没有任何供boolean值专用的字节码指令 所以 boolean平时会被转为int类型 所以占用4个字节，此处boolean的数组会被转为byte数组，所以占用一个字节
+        //在Java虚拟机中没有任何供boolean值专用的字节码指令，Java语言表达式所操作的boolean值，在编译之后都使用Java虚拟机中的int数据类型来代替
         boolean[] array = new boolean[1024*1024];
     }
     static void testObject(){
